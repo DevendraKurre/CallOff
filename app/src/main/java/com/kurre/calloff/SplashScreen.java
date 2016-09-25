a774d5b57d03788f4c3d531d4cbdba2f656e8657
Devendra kumar kurre <devendra.kurre@nokia.com>
2016-09-25 15:08:40 +0530

    initial project only with splashscreen

---
+++ app/src/main/java/com/kurre/calloff/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.kurre.calloff;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //Java way of doing it
+        Thread thread = new Thread()
+        {
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                    //Intent i=new Intent(SplashScreen.this,Login.class);
+                    //startActivity(i);
+                    //finish();       //will finish spash activity
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+
+        //Android way of doing this
+        /*
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashScreen.this,Login.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+        */
+    }
+}
