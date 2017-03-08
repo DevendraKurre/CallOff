d356f19f3afa02461a1ce8acedb1c1e98ccae97e
Devendra kumar kurre <devendrakurre@yahoo.com>
2017-03-08 11:57:19 +0530

    Adding test case for date conversion
    Adding test case for date conversion

--- app/src/test/java/com/kurre/calloff/ExampleUnitTest.java
+++ app/src/test/java/com/kurre/calloff/ExampleUnitTest.java
@@ -1,6 +1,15 @@
 package com.kurre.calloff;
 
+import android.os.Environment;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
@@ -9,7 +18,19 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void test_socket() {
+        System.out.println(new Date().toString());
+
+        String date = "Tue Nov 01 08:52:08 IST 2016";
+        String[] time = date.split(" ")[3].split(":");
+        String timestamp = time[0] + ":" + time[1] + " " + (Integer.valueOf(time[0]) < 12 ? "am" : "pm");
+        System.out.println(timestamp);
+        /*try {
+            new SimpleDateFormat("E M d H:m:s z YYYY").parse("Tue Nov 01 08:52:08 IST 2016");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }*/
+        //Date date = new Date("Tue Nov 01 08:52:08 IST 2016");
+        //System.out.println(new SimpleDateFormat("h:mm a").format(date));
     }
 }
